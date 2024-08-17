f02a3073c11da93719261f301c23fa2f241172bf
nishantcg13 <galandesonu42@gmail.com>
2024-08-17 23:23:38 +0530

    multi of two numbers

--- Interns-Code/Main.js
+++ Interns-Code/Main.js
@@ -5,6 +5,24 @@ class Main {
 	console.log("Hii my name is Nishant");
 
     }
+
+
+
+    
+
+
+
+
+
+
+
+
+    static multi(){
+	let a = 5;
+	let b = 6;
+	console.log("addition is "+(a*b));
+    }
 }
 
 Main.name();
+Main.multi();
