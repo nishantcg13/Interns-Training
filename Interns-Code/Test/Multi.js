816560afe2a7654d87ebd3627b596a40bd9c0350
nishantcg13 <galandesonu42@gmail.com>
2024-08-18 11:17:28 +0530

    multiplication

---
+++ Interns-Code/Test/Multi.js
@@ -0,0 +1,28 @@
+class Main {
+    static name() {
+   
+
+	console.log("Hii my name is Nishant");
+
+    }
+
+
+
+    
+
+
+
+
+
+
+
+
+    static multi(){
+	let a = 5;
+	let b = 6;
+	console.log("addition is "+(a*b));
+    }
+}
+
+Main.name();
+Main.multi();
